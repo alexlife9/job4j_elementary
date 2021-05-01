01a6439967794b6d5088d49a7fcee4984bc66965
Alexlife9 <crashmx@inbox.ru>
2021-05-01 17:22:37 +0300

    5. Конвертер валюты [#41583 #344]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,21 +2,23 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static double rubleToEuro(int value) {
-        double rsl = value / 70.0;
-        return value;
+    public static double rubleToEuro(int rubles) {
+        double kurs_euro = 70.0;
+        double rsl = rubles / kurs_euro;
+        return rsl;
     }
 
-    public static double rubleToDollar(int value) {
-        double rsl = value / 60.0; /* формула перевода рублей в доллары. */
-        return value;
+    public static double rubleToDollar(int rubles) {
+        double kurs_dollar = 60.0;
+        double rsl = rubles / kurs_dollar;
+        return rsl;
     }
 
     public static void main(String[] args) {
         int rubles = 140;
         double euro = Converter.rubleToEuro(rubles);
         double dollar = Converter.rubleToDollar(rubles);
-        System.out.println("140 рублей это " + euro + " euro при курсе 70р за 1 евро");
-        System.out.println("140 рублей это " + dollar + " dollar при курсе 60р за один евро");
+        System.out.println(rubles + " рублей это " + euro + " евро при курсе 70р за 1 евро");
+        System.out.println(rubles + " рублей это " + dollar + " доллар при курсе 60р за один евро");
     }
 }
