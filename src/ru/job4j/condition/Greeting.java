2d5808de7a457212791d4a12c0226d6cecfc57da
Alexlife9 <crashmx@inbox.ru>
2021-05-01 22:06:06 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        String idea2 = "But I am a newbie.";
+        String separate = " ";
+        int year = 2021;
+        System.out.println(idea + separate + idea2 + separate + "1 мая " + year);
+
+
+    }
+}
