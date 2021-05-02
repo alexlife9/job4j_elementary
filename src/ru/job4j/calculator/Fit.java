dff0f4b327ab7fa9391380714d19d89a50f977b0
Alexlife9 <crashmx@inbox.ru>
2021-05-02 17:21:35 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,20 +2,20 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double manWeight(short height) {
-        return (height - 100) * 1.15;
+    public static double manWeight(short heightM) {
+        return (heightM - 100) * 1.15;
     }
 
-    public static double womanWeight(short height1) {
-        return (height1 - 110) * 1.15;
+    public static double womanWeight(short heightW) {
+        return (heightW - 110) * 1.15;
     }
 
     public static void main(String[] args) {
-        short height = 187;
-        short height1 = 160;
-        double man = Fit.manWeight(height);
-        double woman = Fit.womanWeight(height1);
-        System.out.println("Man 187 is " + man);
+        short heightM = 187;
+        short heightW = 150;
+        double man = Fit.manWeight(heightM);
+        double woman = Fit.womanWeight(heightW);
+        System.out.println("Мужчина при росте " + heightM + "см должен весить " + man + "кг");
         System.out.println("Woman 160 is " + woman);
     }
 }
