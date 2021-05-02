696deea9fc043674a46c6e33d3db23a61d290c3d
Alexlife9 <crashmx@inbox.ru>
2021-05-02 17:33:27 +0300

    1. Модульные тесты. [#159435 #397]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -8,13 +8,17 @@ public class ConverterTest {
 
     @Test
     public void whenConvert140RblThen2Euro() {
-        int rubles = 150;
-        double expectedE = 1.5;
+        int rubles = 140;
+        double expectedE = 2;
         double euro = Converter.rubleToEuro(rubles);
-        Assert.assertEquals(expectedE, euro);
+        Assert.assertEquals(expectedE, euro, 0.01);
     }
 
     @Test
     public void whenConvert180RblThen3Dlr() {
+        int rubles = 180;
+        double expectedD = 3;
+        double dollar = Converter.rubleToDollar(rubles);
+        Assert.assertEquals(expectedD, dollar, 0.01);
     }
 }
