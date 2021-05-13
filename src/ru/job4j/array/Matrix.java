3774779a4dfafea63486d90e14262fd8111b82d0
Alexlife9 <crashmx@inbox.ru>
2021-05-13 13:57:32 +0300

    6.6.2. Двухмерный массив. Циклы. [#306725 #1270220]

---
+++ src/ru/job4j/array/Matrix.java
@@ -0,0 +1,18 @@
+/*
+package ru.job4j.array;
+
+public class Matrix {
+
+    public static int[][] multiple(int size) {
+        int rsl = 0;
+        for (int row = 0; row < size.length; row++) {
+            for (int cell = 0; cell < size.length; cell++) {
+                if
+                }
+
+            }
+        }
+        return rsl;
+    }
+}
+*/
--- src/ru/job4j/array/MatrixSum.java
+++ src/ru/job4j/array/MatrixSum.java
@@ -5,7 +5,7 @@ public class MatrixSum {
     public static int sum(int[][] array) {
         int rsl = 0;
         for (int row = 0; row < array.length; row++) {
-            for (int cell = 0; cell < array.length; cell++) {
+            for (int cell = 0; cell < array[row].length; cell++) { //извлекаем вложенный массив по индексу и определяем его длину, вызывая поле length
                 int val = array[row][cell];
                 rsl += val;
             }
