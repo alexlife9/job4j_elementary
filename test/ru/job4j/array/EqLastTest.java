548cf0ba2e76525fa047df6ea9fdfcfdd935d984
Alexlife9 <crashmx@inbox.ru>
2021-05-14 19:09:36 +0300

    разное

--- test/ru/job4j/array/EqLastTest.java
+++ test/ru/job4j/array/EqLastTest.java
@@ -1,22 +1,22 @@
-//package ru.job4j.array;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class EqLastTest {
-//    @Test
-//    public void whenEq() {
-//        int[] left = {1, 2, 3};
-//        int[] right = {5, 4, 3};
-//        boolean result = EqLast.check(left, right);
-//        Assert.assertTrue(result);
-//    }
-//
-//    @Test
-//    public void whenNotEq() {
-//        int[] left = {1, 2, 3};
-//        int[] right = {3, 2, 4};
-//        boolean result = EqLast.check(left, right);
-//        Assert.assertFalse(result);
-//    }
-//}
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EqLastTest {
+    @Test
+    public void whenEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {5, 4, 3};
+        boolean result = EqLast.check(left, right);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenNotEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {3, 2, 4};
+        boolean result = EqLast.check(left, right);
+        Assert.assertFalse(result);
+    }
+}
