b1be5db2855308a9bfa65f083e802870eb13ad14
Alexlife9 <crashmx@inbox.ru>
2021-05-15 13:16:59 +0300

    6.8. Дефрагментация массива [#225 #1384]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -2,20 +2,21 @@ package ru.job4j.array;
 
 public class Defragment {
     public static String[] compress(String[] array) {
-        for (int index = 0; index < array.length; index++) {
-            if (array[index] == null) {
-                int point = index; /* указатель на null ячейку. */
-                for (int i = 0; i < array.length; i++) {
-                    if (array[i] != null) {
+        for (int index = 0; index < array.length; index++) { //узнаем длину массива строк
+            if (array[index] == null) { //если значение индекса равно 0, то
+                int point = index; // указатель на null ячейку. вводим переменную  point со значением index
+                for (int i = point + 1; i < array.length; i++) { // для массива от null (и смотрим следующий индекс +1) узнаем длину массива оставших значений строк
+                    if (array[i] != null) { // если значение индекса нового массива не равно null, то меняем местами null и найденное значение
                         String temp = array[i];
                         array[i] = array[index];
                         array[index] = temp;
+                        break; // останавливаем замен
                     }
                 }
             }
             System.out.print(array[index] + " ");
         }
-        return array;
+        return array; //запускаем цикл заново
     }
 
     public static void main(String[] args) {
--- test/ru/job4j/array/DefragmentTest.java
+++ test/ru/job4j/array/DefragmentTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DefragmentTest {
+
     @Test
     public void singleFirstNull() {
         String[] input = {null, "I"};
