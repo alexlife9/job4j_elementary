124250c170e0d52c03da2adcee9cdcc1fa93a623
Alexlife9 <crashmx@inbox.ru>
2021-05-20 14:08:29 +0300

    1.6. Взаимодействие объектов [#166087 #158413]

---
+++ src/ru/job4j/oop/BallStory.java
@@ -0,0 +1,44 @@
+package ru.job4j.oop;
+
+public class BallStory {
+
+    public class Ball {
+
+        public void run1(Hare hare) {
+        }
+
+        public void run2(Wolf wolf) {
+        }
+
+        public void run3(Fox fox) {
+        }
+    }
+
+    public class Hare {
+        public void tryEat(Ball ball) {
+        }
+    }
+
+    public class Wolf {
+        public void tryEat(Ball ball) {
+        }
+    }
+
+    public class Fox {
+        public void tryEat(Ball ball) {
+        }
+    }
+
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+        ball.run1(hare);
+        ball.run2(wolf);
+        ball.run3(fox);
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
+        fox.tryEat(ball);
+    }
+}
---
+++ src/ru/job4j/other/Temp.java
@@ -0,0 +1,10 @@
+package ru.job4j.other;
+
+public class Temp {
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 2, 4, 5, 9, 7874};
+        for (int item: array) {
+            System.out.println(item);
+        }
+    }
+}
