2f963caa4178407c748a285e6452444987024f62
Alexlife9 <crashmx@inbox.ru>
2021-05-23 02:20:43 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,7 +1,18 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left > right ? left : right;
+
+    public static int add(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public int add(int first, int second, int third) {
+        int tmp = add(second, third);
+        return add(first, tmp);
+    }
+
+    public int add(int first, int second, int third, int fourth) {
+        int tmp = add(second, third, fourth);
+        return add(first, tmp);
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -6,27 +6,27 @@ import org.junit.Test;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.add(first, second);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax2To1Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.add(first, second);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 2;
+        int second = 2;
+        int result = Max.add(first, second);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
