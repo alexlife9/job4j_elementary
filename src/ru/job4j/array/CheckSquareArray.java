9b1efe1dfbfa9ed7eb832a7abd29f80cf04bc00f
Alexlife9 <crashmx@inbox.ru>
2021-06-04 21:44:00 +0300

    12. Проверить является ли двумерный массив квадратным

---
+++ src/ru/job4j/array/CheckSquareArray.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class CheckSquareArray {
+    public static boolean checkArray(int[][] array) {
+        boolean rsl = true;
+        int standard = array.length;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != standard) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
+/*
+Проверить является ли входящий двумерный массив квадратным
+(т.е. количество элементов в каждом ряду, должно совпадать с количеством рядов)
+*/
